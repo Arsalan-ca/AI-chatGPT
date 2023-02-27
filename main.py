cef6e12769e764cf8a4806bc6345043a4e5bb848
Arsalan-ca <arsalanmod2003@gmail.com>
2023-02-26 22:23:46 -0500

    first

---
+++ main.py
@@ -0,0 +1,43 @@
+import speech_recognition as sr
+import openai
+import pyttsx3
+
+situation = True
+
+openai.api_key = "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"
+model_engine = "text-davinci-003"
+
+## this is text to speech
+text_speech = pyttsx3.init()
+
+
+## writing the voice recognizer
+
+def voice():
+    print("Say something...")
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        audio = r.listen(source)
+        command = r.recognize_google(audio)
+        command = command.lower()
+    return command
+
+
+def chat_answer():
+    command = voice()
+    completion = openai.Completion.create(
+        engine=model_engine,
+        prompt=command,
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+    response = completion.choices[0].text
+    print(response)
+    text_speech.say(response)
+    text_speech.runAndWait()
+
+
+while situation:
+    chat_answer()
